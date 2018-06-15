8a1d602786c7c8e772411b9e0306bd26295f10f2
irreality4free <irreality4free@gmail.com>
2018-06-15 14:56:33 +0300

    parcer

---
+++ parser.py
@@ -0,0 +1,71 @@
+# -*-coding:utf-8 -*-
+from struct import *
+import codecs
+
+
+class Parser:
+    def __init__(self,name):
+        self.name = name
+        self.j = 0
+        self.element = 0
+        self.layer = 0
+        self.layers = dict()
+
+    def Open(self):
+        with open(self.name, "r", encoding='cp866') as  f:
+            self.s = f.read()
+            self.s = self.s.split("$$HEADEREND")[1]
+            self.s = self.s.encode('cp866')
+
+    def get_command(self,s):
+
+
+        c = unpack("H", s[:2])[0]
+
+        s = s[2:]
+        print(c)
+        if c == 128:  # layer
+            self.layer += 1
+            self.layers[str(self.layer)] = list()
+            print(str(self.layer) + ' layer num')
+            z = unpack("H", s[:2])
+            s = s[2:]
+            element = 0
+            print("Start layer (128) %s" % z)
+        elif c == 129:
+            id_ = unpack("H", s[:2])
+            s = s[2:]
+            dir_ = unpack("H", s[:2])
+            s = s[2:]
+            n = unpack("H", s[:2])[0]
+            s = s[2:]
+            print("Start polyline (129) id: %s; dir: %s; n: %s;" % (id_, dir_, n))
+            coord_list = list()
+            for i in range(n):
+                x, y = unpack("HH", s[:4])
+
+                s = s[4:]
+                x_scaled, y_scaled = int(float(x) / 65536 * 10000 - 1100), int(float(y) / 65536 * 10000 - 1100)
+
+                coord_list.append(x_scaled)
+                coord_list.append(y_scaled)
+
+                print(x_scaled, y_scaled)
+            print()
+            self.layers[str(self.layer)].append(coord_list)
+            self.element += 1
+        else:
+
+            return ""
+        return s
+
+    def Run(self):
+        self.Open()
+        while len(self.s) > 0:
+            self.s = self.get_command(self.s)
+        print(self.layers['1'])
+
+
+par = Parser("s_11zzz1_ex.cli")
+par.Run()
+
